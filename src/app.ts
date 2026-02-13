f07ad1b7c14c9cc37326265a7a0a8ebd4823f26b
Natarajan Shanmugam <natrajan.raj@gmail.com>
2026-02-13 21:59:54 +0530

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
 import express from "express";
 import cors from "cors";
+import {sequelize} from "./config/database";
 import userRoutes from "./routes/user";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,5 +17,23 @@ app.use("/api/users", userRoutes);
 app.get("/api/health", (_, res) => {
   res.json({ status: "API running 🚀" });
 });
+
+// DB connection
+async function startServer() {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected successfully.");
+
+    await sequelize.sync(); // Sync models
+
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  } catch (error) {
+    console.error("Unable to connect to DB:", error);
+  }
+}
+
+startServer();
 
 export default app;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,13 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import User from "../models/user";
 import { Sequelize, DataTypes } from 'sequelize';
+import { authMiddleware } from "../auth/auth";
+import { login } from "../controller/auth.controller";
+import { verifyToken, JwtPayload } from "../utils/jwt";
+
+export interface AuthRequest extends Request {
+  user?: JwtPayload;
+}
 
 const router = Router();
 
@@ -9,25 +16,29 @@ router.post("/", async (req, res) => {
   res.json(user);
 });
 
-router.get("/", async (_, res) => {
-  const users = await User.findAll();
+router.get("/", authMiddleware, async (req: AuthRequest, res) => {
+
+  console.log("Logged user:", req.user);
+
+  const users = await User.findAll({ where: { email: req.user?.email }, raw: true });
   res.json(users);
 });
 
+router.post("/login", login);
 
 router.post("/update", async (_, res) => {
 
-  try{
 
-  const user_res = await User.create({
-    name: 'natty',
-    email: 'natty@gmail.com',
-  });
+  try {
+    const user_res = await User.create({
+      name: 'test',
+      email: 'test@gmail.com',
+    });
 
-  res.json(user_res);
-} catch (error){
-  console.log('error: ', error)
-}
+    res.json(user_res);
+  } catch (error) {
+    console.log('error: ', error)
+  }
 });
 
 export default router;
